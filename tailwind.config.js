e07b153c258b615964a6b423362b67a92a7d10cd
Orothisz <112760300+Orothisz@users.noreply.github.com>
2025-08-11 09:59:49 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        midnight: "#000026",
+        gold: "#eab308",
+        indigo: "#6366f1"
+      }
+    }
+  },
+  plugins: []
+};
